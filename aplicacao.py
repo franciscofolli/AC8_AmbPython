1715d0585aada00a15c149f9359f17214e0c4e7f
franciscofolli <franciscoabel2001@gmail.com>
2019-10-23 20:26:08 -0300

    Senhores, estou subindo o arquivo 'aplicacao.py' que contem a referencia de todo o site quando for ativo e aberto o ambiente, ja fiz todas as referencias e vocês irão fazer a ligacao do html com o py, nomeando no cod do html nomes extamente como estao os arquivos

---
+++ aplicacao.py
@@ -0,0 +1,43 @@
+from flask import flask, render_template
+# notações de variáveis
+#f --> função
+#c --> caracter/string
+#ft --> float
+#a --> array
+#li --> lista 
+
+app = Flask(__name__)
+
+#Pagina inicial
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#page cursos
+@app.route('/cursos')
+def curs():
+    return render_template('cursos.html')
+
+#page detalhes dos cursos/faculdade
+@app.route('/detalhes')
+def detalhes():
+    return render_template('detalhes.html')
+
+#page disciplinas
+@app.route('/disciplinas')
+def detalhes():
+    return render_template('disciplinas.html')
+
+#page noticias
+@app.route('/noticias')
+def detalhes():
+    return render_template('noticias.html')
+
+app.run()
+
+
+
+
+
+
+
